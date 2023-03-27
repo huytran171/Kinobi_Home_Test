1e8ccda4c24cad00e38423abd78110d07e3ed5a5
huytran <huytv@mor.com.vn>
2023-03-27 14:16:44 +0700

    update

--- server/src/middlewares/file-upload.ts
+++ server/src/middlewares/file-upload.ts
@@ -27,7 +27,7 @@ export default function makeMulterS3FileUpload() {
     storage: multerS3({
       s3,
       acl: "public-read",
-      bucket: process.env.S3_BUCKET,
+      bucket: "huytv",
       key: function (req, file, cb) {
         cb(null, Date.now().toString());
       },
